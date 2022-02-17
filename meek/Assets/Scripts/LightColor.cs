8b5550ff3d183f470ac5c71a364358f053bb3ea4
xiasantos <sofia.fls.santos@gmail.com>
2022-02-17 23:14:08 +0000

    lights working
    todo:fix colors because xico ruined my beautiful colors :(
    make start screen
    make end screen

---
+++ meek/Assets/Scripts/LightColor.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using UnityEngine;
+
+public class LightColor : MonoBehaviour
+{
+    private Coroutine cycling = null;
+    private int i = 0;
+    private int j = 1;
+
+    public Light sunLight;
+    public Color[] dayCycles;
+    public int[] cycleDuration;
+
+    private void Start()
+    {
+        sunLight = GetComponent<Light>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (cycling == null)
+        {
+            cycling = StartCoroutine(
+                NextLightCycle(
+                    sunLight, dayCycles[i], dayCycles[j], 3
+                )
+            );
+        }
+    }
+
+
+    private IEnumerator NextLightCycle(Light targetLight, Color from, Color to, float duration)
+    {
+        var timer = 0f;
+
+        while (timer <= duration)
+        {
+            timer += Time.deltaTime;
+            targetLight.color = Color.Lerp(from, to, timer / duration);
+
+            yield return null;
+        }
+
+        targetLight.color = to;
+
+        yield return new WaitForSeconds(cycleDuration[i]);
+
+        i = (i + 1) % dayCycles.Length;
+        j = (j + 1) % dayCycles.Length;
+        cycling = null;
+    }
+}
